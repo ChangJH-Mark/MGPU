e130739037f74c1821f3e561f7e595eed89ed7ae
markchang <markchang@buaa.edu.cn>
2021-04-20 21:47:58 +0800

    1.fix(cmake): make cuda root indenpendent

--- test/C++Utility/struct_initialize_test.cpp
+++ test/C++Utility/struct_initialize_test.cpp
@@ -1,23 +1,43 @@
-//
-// Created by root on 2021/4/19.
-//
-
 #include <iostream>
+#include <map>
+#include <unordered_map>
 using namespace std;
 
-typedef struct Node {
-    int a = 1;
-    int b = 1;
-    int c = 1;
-    void print() {
-        cout << "a : " << a << " b: " << b << " c: " << c << endl;
-    }
-} Node;
-
 int main() {
-    Node default_;
-    default_.print();
-
-    Node init_with_value {4, 5};
-    init_with_value.print();
+    const char * res[4] = {"xiaoming", "xiaowang", "xiaoli", "draw"};
+    int winner = 3, win_value = -1;
+    int n;
+    map<int,string> str;
+    cin >> n;
+    cin >> str[0] >> str[1] >> str[2];
+    for(int i = 0;i < 3; i++)
+    {
+        int max_num = 0;
+        unordered_map<char, int> appeared;
+        // get every string's most often char
+        for(auto iter = str[i].begin(); iter != str[i].end(); iter++)
+        {
+            if(appeared.count(*iter))
+            {
+                appeared[*iter] ++;
+                max_num = std::max(max_num, appeared[*iter]);
+            } else {
+                appeared[*iter] = 1;
+                max_num = max_num?max_num: 1;
+            }
+        }
+        int final = std::min(static_cast<unsigned long >(max_num + n), str[i].size());
+        int extra = (final ==str[i].size()) ? (max_num + n - str[i].size()) % 2 : 0;
+        final -= extra;
+        if(final > win_value)
+        {
+            winner = i;
+            win_value = final;
+        } else if(final == win_value)
+        {
+            winner = 3;
+            break;
+        }
+    }
+    cout << res[winner] << endl;
 }
