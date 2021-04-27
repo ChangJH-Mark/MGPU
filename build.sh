3ea05c20b16b9aa1389d08617d25ec0416d670b3
ChangJH-Mark <1431293514@qq.com>
2021-04-27 17:56:05 +0800

    1.test(sample): add hotspot(com), pathfinder(com), streamcluster(mem) benchmark

--- build.sh
+++ build.sh
@@ -7,7 +7,9 @@ fi
 if [ ! -d /opt/custom ]
 then
 	mkdir -p /opt/custom/ptx
+	mkdir -p /opt/custom/bin/test
 fi
 
-cmake -DCMAKE_BUILD_TYPE=DEBUG -DCppUTILITY=ON -H. -Bbuild
-cmake --build build -- -j 12 
+cmake -DCMAKE_BUILD_TYPE=DEBUG -DCppUTILITY=ON -DBUILD_TEST=ON -H. -Bbuild
+cmake --build build -- -j 12
+cmake -P build/cmake_install.cmake
