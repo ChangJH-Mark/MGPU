5630c137af52d38e86b8f0efa02806f0797f90e8
ChangJH-Mark <1431293514@qq.com>
2021-11-23 21:45:00 +0800

    1.feat(lab3): add lab3 test

--- test/client/test_client.cpp
+++ test/client/test_client.cpp
@@ -5,29 +5,14 @@
 #include <unistd.h>
 #include <chrono>
 #include <assert.h>
+#include <atomic>
+#include <cuda.h>
 #include "client/api.h"
 
 using namespace std;
 
-void test_sm() {
-    uint block_num = 10;
-    uint size = sizeof(block_num) * block_num;
-    void *dev_ptr1 = mgpu::cudaMalloc(size);
-    void *host_ptr1 = mgpu::cudaMallocHost(size);
-    mgpu::stream_t stream;
-    mgpu::cudaStreamCreate(&stream);
-    mgpu::cudaLaunchKernel({{block_num}, {1}, 0, stream}, "/opt/custom/ptx/specify_sm.ptx", "sm_ids", dev_ptr1);
-    mgpu::cudaStreamSynchronize(stream);
-    mgpu::cudaMemcpy(host_ptr1, dev_ptr1, size, cudaMemcpyDeviceToHost);
-    for (int i = 0; i < block_num; i++) {
-        printf("%d ", *((int *) (host_ptr1) + i));
-    }
-    printf("\n");
-    mgpu::cudaFree(dev_ptr1);
-    mgpu::cudaFreeHost(host_ptr1);
-}
-
-void test_vectorAdd() {
+void test_vectorAdd()
+{
     const int N = 1 << 24;
     void *dev_ptr1 = mgpu::cudaMalloc(N);
     void *dev_ptr2 = mgpu::cudaMalloc(N);
@@ -39,16 +24,59 @@ void test_vectorAdd() {
                            dev_ptr2, dev_ptr2,
                            int(N / sizeof(int)));
     mgpu::cudaMemcpy(host_ptr, dev_ptr2, N, cudaMemcpyDeviceToHost);
-//    for(int i = 0; i < N/sizeof(int); i++) {
-//        printf("%d: 0x%x\n", i, ((int *)(host_ptr))[i]);
-//    }
-    printf("0x%x\n", *((int *) host_ptr + N / sizeof(int) - 1));
+    //    for(int i = 0; i < N/sizeof(int); i++) {
+    //        printf("%d: 0x%x\n", i, ((int *)(host_ptr))[i]);
+    //    }
+    printf("0x%x\n", *((int *)host_ptr + N / sizeof(int) - 1));
     mgpu::cudaFree(dev_ptr1);
     mgpu::cudaFree(dev_ptr2);
     mgpu::cudaFreeHost(host_ptr);
 }
 
-void test_matrixMul() {
+chrono::steady_clock::time_point s;
+chrono::steady_clock::time_point e;
+
+atomic<int> cnts(0);
+
+void vectorAdd(char *host_ptr1, char *host_ptr2, int dev)
+{
+    ::cudaSetDevice(dev);
+    const int N = 1 << 24;
+    void *dev_ptr1, *dev_ptr2, *dev_ptr3;
+
+    memset(host_ptr1, 0x1, N);
+    memset(host_ptr2, 0x2, N);
+
+    cudaCheck(cudaMalloc(&dev_ptr1, N));
+    cudaCheck(cudaMalloc(&dev_ptr2, N));
+    cudaCheck(cudaMalloc(&dev_ptr3, N));
+    cudaCheck(cudaMemcpy(dev_ptr1, host_ptr1, N, cudaMemcpyHostToDevice));
+    cudaCheck(cudaMemcpy(dev_ptr2, host_ptr2, N, cudaMemcpyHostToDevice));
+
+    void *host_ptr;
+    CUmodule mod;
+    CUfunction func;
+    cudaStream_t stream;
+    cudaCheck(cudaStreamCreate(&stream));
+    cudaCheck(cuModuleLoad(&mod, "/opt/custom/ptx/vectorAdd.cubin"));
+    cudaCheck(cuModuleGetFunction(&func, mod, "vectorAdd"));
+    char param[1024];
+    int size = fillParameters(param, 0, dev_ptr1, dev_ptr2, dev_ptr3, N / sizeof(int));
+    void *extra[] = {
+            CU_LAUNCH_PARAM_BUFFER_POINTER, param,
+            CU_LAUNCH_PARAM_BUFFER_SIZE, &size,
+            CU_LAUNCH_PARAM_END};
+    dim3 grid = (N / 1024);
+    dim3 block = 256;
+    cudaCheck(cuLaunchKernel(func, grid.x, grid.y, grid.z, block.x, block.y, block.z, 0, stream, NULL, extra));
+    cudaCheck(cudaStreamSynchronize(stream));
+    cudaCheck(cudaMemcpy(host_ptr2, dev_ptr3, N, cudaMemcpyDeviceToHost));
+    // cudaCheck(cudaFree(dev_ptr1));
+    // cudaCheck(cudaFree(dev_ptr2));
+}
+
+void test_matrixMul()
+{
     const int block_size = 16;
     const int wA = 5 * 2 * block_size, hA = wA;
     const int wB = 5 * 4 * block_size, hB = wA;
@@ -78,7 +106,8 @@ void test_matrixMul() {
     mgpu::cudaFree(matA);
     mgpu::cudaFree(matB);
     mgpu::cudaFree(matC);
-    for (int i = 0; i < block_size; i++) {
+    for (int i = 0; i < block_size; i++)
+    {
         for (int j = 0; j < block_size; j++)
             cout << static_cast<float *>(h_C)[i * block_size + j] << "\t";
         cout << endl;
@@ -86,45 +115,23 @@ void test_matrixMul() {
     mgpu::cudaFreeHost(h_C);
 }
 
-void test_multiGPUmatrixMul() {
-    const int block_size = 16;
-    const int wA = block_size, hA = wA;
-    const int wB = block_size, hB = wA;
-    void *h_A = mgpu::cudaMallocHost(sizeof(float) * wA * hA);
-    void *h_B = mgpu::cudaMallocHost(sizeof(float) * wB * hB);
-    // initial matrix
-    for (int i = 0; i < wA * hA; i++)
-        static_cast<float *>(h_A)[i] = 1.0f;
-    for (int i = 0; i < wB * hB; i++)
-        static_cast<float *>(h_B)[i] = 0.1f;
-    dim3 threads(block_size, block_size);
-    dim3 grid(wB / threads.x, hA / threads.y);
-    auto res = mgpu::matrixMul_MGPU({h_A, wA, hA}, {h_B, wB, hB}, {grid, threads});
-    auto value = static_cast<float *>(res.get());
-    if (!value)
-        exit(1);
-    for (int i = 0; i < hA; i++) {
-        for (int j = 0; j < wB; j++) {
-            cout << value[i * wA + j] << " ";
-        }
-        cout << endl;
-    }
-    mgpu::cudaFreeHost(h_A);
-    mgpu::cudaFreeHost(h_B);
-}
-
-void test_multiGPU() {
+void test_multiGPU()
+{
     const int N = 1 << 24;
     const int TaskNum = 6;
     mgpu::Task tasks[TaskNum];
+    vector<void *> ptrs;
     char *h_ptr_1, *h_ptr_2;
-    for (auto & task : tasks) {
+    for (auto &task : tasks)
+    {
         task.hdn = 2;
         task.hds[0] = N;
         task.hds[1] = N;
 
         h_ptr_1 = (char *)mgpu::cudaMallocHost(N);
         h_ptr_2 = (char *)mgpu::cudaMallocHost(N);
+        ptrs.push_back(h_ptr_1);
+        ptrs.push_back(h_ptr_2);
         memset(h_ptr_1, 0x1, N);
         memset(h_ptr_2, 0x2, N);
         task.p_size = fillParameters(task.param, 0, h_ptr_1, h_ptr_2, (void *)NULL, N / sizeof(int));
@@ -133,26 +140,56 @@ void test_multiGPU() {
         task.dev_alloc_size[0] = N;
         strcpy(task.ptx, "/opt/custom/ptx/vectorAdd.cubin");
         strcpy(task.kernel, "vectorAdd");
-        task.conf = {.grid = {(N/sizeof(int) / 256) + 1},.block= 256};
+        task.conf = {.grid = {(N / sizeof(int) / 256) + 1}, .block = 256};
     }
+    chrono::steady_clock::time_point start = chrono::steady_clock::now();
     auto ret = mgpu::MulTaskMulGPU(TaskNum, tasks);
-    for(int i = 0;i<TaskNum;i++) {
-        cout << "task " << i << " success is " << ret.success[i] << " dev: " << ret.bind_dev[i] << endl;
+    for (int i = 0; i < TaskNum; i++)
+    {
         void *result = (void *)*(unsigned long long *)(ret.msg[i] + 2 * sizeof(void *));
-        cout << "start check result ptr " << hex << result << dec << endl;
         mgpu::cudaMemcpy(h_ptr_1, result, N, cudaMemcpyDeviceToHost);
-        for(int j = 0; j< N;j++) {
-            if(h_ptr_1[j] != 0x03)
-                cout << "for task " << i << " result " << j << " is not 0x03 but " << hex << (int)h_ptr_1[j] << dec << endl;
-        }
+    }
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    cout << " total cost " << chrono::duration_cast<chrono::microseconds>(end - start).count() << endl;
+    for (int i = 0; i < TaskNum; i++)
+    {
+        cout << "task" << i << " chose " << ret.bind_dev[i] << endl;
+    }
+    for (auto p : ptrs)
+    {
+        mgpu::cudaFreeHost(p);
     }
 }
 
-int main() {
-//    test_vectorAdd();
-//    test_matrixMul();
-    test_multiGPU();
-//    test_multiGPUmatrixMul();
-    //test_sm();
+int main()
+{
+    struct example
+    {
+        char *host_ptr1, *host_ptr2;
+        int dev;
+    } E[6];
+    int id = 0;
+    srand(time(NULL));
+    for (int i = 0; i < 6; i++)
+    {
+        id = 0;
+        cudaSetDevice(id);
+        cudaFree(0);
+        cudaCheck(cudaMallocHost(&(E[i].host_ptr1), 1 << 24));
+        cudaCheck(cudaMallocHost(&(E[i].host_ptr2), 1 << 24));
+        E[i].dev = id;
+        cout << id << endl;
+    }
+    thread ts[6];
+    chrono::steady_clock::time_point start = chrono::steady_clock::now();
+    for (int i = 0; i < 6; i++)
+    {
+        ts[i] = std::move(thread(vectorAdd, E[i].host_ptr1, E[i].host_ptr2, E[i].dev));
+    }
+    for (int i = 0; i < 6; i++)
+        ts[i].join();
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    cout << " total cost " << chrono::duration_cast<chrono::microseconds>(end - start).count() << endl;
+    // test_multiGPU();
     return 0;
 }
